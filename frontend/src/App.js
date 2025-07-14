610986ad55a3f900eff16e64eb8a31c87d144aec
Nirmana Herath <nirmanaherath123@gmail.com>
2025-07-14 23:05:25 +0530

    properly add frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to BookUs</h1>
+      <p>This is your appointment booking system.</p>
+    </div>
+  );
+}
+
+export default App;
